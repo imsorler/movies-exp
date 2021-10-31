6719855dcee378658e7f7cad2f561a2695fd5b3d
imsorler <imsorler@gmail.com>
2021-10-31 12:56:46 +0300

    added auth on promises

--- src/components/Header/Login/Login.jsx
+++ src/components/Header/Login/Login.jsx
@@ -3,38 +3,63 @@ import { API_URL, API_KEY_3 } from '../../../api/api';
 
 export default class Login extends Component {
   sendPromises = () => {
-    fetch(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
-      .then((res) => res.json())
+    const fetchApi = (url, options = {}) => {
+      return new Promise((resolve, reject) => {
+        fetch(url, options)
+          .then((resolve) => {
+            if (resolve.status < 400) {
+              return resolve.json();
+            } else {
+              throw resolve;
+            }
+          })
+          .then((data) => {
+            resolve(data);
+          })
+          .catch((err) => {
+            err.json().then((e) => {
+              reject(e);
+              console.log('Error_21: ', e);
+            });
+          });
+      });
+    };
+
+    fetchApi(`${API_URL}/authentication/token/new?api_key=${API_KEY_3}`)
       .then((data) => {
-        fetch(`${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`, {
+        return fetchApi(
+          `${API_URL}/authentication/token/validate_with_login?api_key=${API_KEY_3}`,
+          {
+            method: 'POST',
+            mode: 'cors',
+            headers: {
+              'Content-type': 'application/json',
+            },
+            body: JSON.stringify({
+              username: 'sublarki',
+              password: 'neumann213',
+              request_token: data.request_token,
+            }),
+          },
+        );
+      })
+      .then((data) => {
+        return fetchApi(`${API_URL}/authentication/session/new?api_key=${API_KEY_3}`, {
           method: 'POST',
           mode: 'cors',
           headers: {
             'Content-type': 'application/json',
           },
           body: JSON.stringify({
-            username: 'sublarki',
-            password: 'neumann213',
             request_token: data.request_token,
           }),
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            fetch(`${API_URL}/authentication/session/new?api_key=${API_KEY_3}`, {
-              method: 'POST',
-              mode: 'cors',
-              headers: {
-                'Content-type': 'application/json',
-              },
-              body: JSON.stringify({
-                request_token: data.request_token,
-              }),
-            })
-              .then((res) => res.json())
-              .then((data) => {
-                console.log('session', data);
-              });
-          });
+        });
+      })
+      .then((data) => {
+        console.log('session data: ', data);
+      })
+      .catch((err) => {
+        console.log('Error_30:', err);
       });
   };
 
